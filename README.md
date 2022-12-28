561ef82d9266555729db07ce2cc9c069d74350b1
Natalie Agus <nn88blue@gmail.com>
2022-12-28 15:28:02 +0800

    feat: add hooks for theming

--- README.md
+++ README.md
@@ -8,3 +8,47 @@ https://www.youtube.com/watch?v=wYpCWwD1oz0
 # Tailwind shades
 
 Use this to create variation shades from a primary color
+
+# useMemo
+
+`useState` is causing a re-render on the call of the setState method (second element in the array returned). It does **not** have any dependencies like useMemo or useEffect.
+
+`useMemo` only recalculates a value if the elements in its dependency array change (if there are no dependencies - i.e. the array is empty, it will recalculate only once). If the array is left out, it will **recalculate** on every render. Calling the function does not cause a re-render. Also it runs during the render of the component and not before.
+
+useEffect is called after each render, if elements in its dependency array have changed or the array is left out. If the array is empty, it will only be run once on the initial mount (and unmount if you return a cleanup function).
+
+```
+function CoolComponent(props) {
+    const items = useMemo(() =>
+        props.rawItems.map(parseItem);
+    , [props.rawItems]);
+
+    // rest of component...
+}
+```
+
+vs
+
+```
+function CoolComponent(props) {
+    const [items, setItems] = useState();
+
+    useEffect(() => {
+        setItems(props.rawItems.map(parseItem));
+    }, [props.rawItems]);
+
+    // rest of component...
+}
+```
+
+The first one will render the correct item in the beginning.
+The second one will render twice: once with old rawItems, once more with new one. useEffect is ran only after render is complete.
+
+Note: if useMemo does heavy computation, it's better to pass it to useEffect hook because it can be dealt with AFTER something is rendered out already.
+
+Also, using `useMemo()` without dependencies array will calculate the value on every render.
+
+If no array is provided, a new value will be computed on every render.
+
+- It'll be equivalent to `const value = ...`
+- Using `useMemo()` with an empty dependencies array will calculate the value only once, on mount.
--- src/theme.js
+++ src/theme.js
@@ -2,6 +2,7 @@
 
 import { createContext, useState, useMemo } from "react";
 import { createTheme } from "@mui/material/styles";
+import { Moped } from "@mui/icons-material";
 
 // color design tokens
 export const tokens = (mode) => ({
@@ -123,3 +124,97 @@ export const tokens = (mode) => ({
 });
 
 // mui theme settings
+export const themeSettings = (mode) => {
+  const colors = tokens(mode);
+  return {
+    palette: {
+      mode: mode,
+      ...(mode === "dark"
+        ? {
+            // palette values for dark mode
+            primary: {
+              main: colors.primary[500],
+            },
+            secondary: {
+              main: colors.greenAccent[500],
+            },
+            neutral: {
+              dark: colors.grey[700],
+              main: colors.grey[500],
+              light: colors.grey[100],
+            },
+            background: {
+              default: colors.primary[500],
+            },
+          }
+        : {
+            // palette values for light mode
+            primary: {
+              main: colors.primary[100],
+            },
+            secondary: {
+              main: colors.greenAccent[500],
+            },
+            neutral: {
+              dark: colors.grey[700],
+              main: colors.grey[500],
+              light: colors.grey[100],
+            },
+            background: {
+              default: "#fcfcfc",
+            },
+          }),
+    },
+    typography: {
+      fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+      fontSize: 12,
+      h1: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 40,
+      },
+      h2: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 32,
+      },
+      h3: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 24,
+      },
+      h4: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 20,
+      },
+      h5: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 16,
+      },
+      h6: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 14,
+      },
+    },
+  };
+};
+
+// context for color mode
+export const ColorModeContext = createContext({
+  toggleColorMode: () => {},
+});
+
+// custom hook useMode
+export const useMode = () => {
+  const [mode, setMode] = useState("dark");
+
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () =>
+        setMode((prev) => (prev === "light" ? "dark" : "light")),
+    }),
+    []
+  );
+
+  // we need the correct stuff BEFORE things are being rendered
+  // when mode changes, useMemo will recompute the new Theme values (we recompute what themeSettings should return depending on the mode)
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return [theme, colorMode];
+};
