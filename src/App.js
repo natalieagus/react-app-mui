f2feb3472481084fdf814dfa2e515f4f923d9c29
Natalie Agus <nn88blue@gmail.com>
2022-12-28 15:48:36 +0800

    feat: setup all other scenes for later use

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import Topbar from "./scenes/global/Topbar";
+import { Routes, Route } from "react-router-dom";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-
+import Topbar from "./scenes/global/Topbar";
+import Dashboard from "./scenes/dashboard";
+import { Sidebar } from "react-pro-sidebar";
 function App() {
   const [theme, colorMode] = useMode(); //  we have access to the theme and colorMode now
 
@@ -10,8 +12,12 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar />
           <main className="content">
             <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
