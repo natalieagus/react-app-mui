abaf1b454dc73011ac69532974a92e39a12056dd
Natalie Agus <nn88blue@gmail.com>
2022-12-28 16:53:20 +0800

    feat: add Header to be used in dashboard

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Header from "../../components/Header";
+import { Box } from "@mui/system";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItem="center">
+        <Header
+          title="DASHBOARD"
+          subtitle="Welcome to your destination"
+        ></Header>
+      </Box>
+    </Box>
+  );
 };
 
 export default Dashboard;
