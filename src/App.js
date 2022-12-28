86a1972f32de96e880aef8c54603987be8eda810
Natalie Agus <nn88blue@gmail.com>
2022-12-28 15:30:04 +0800

    feat: setup theming in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
 function App() {
-  return <div className="app">Hello world</div>;
+  const [theme, colorMode] = useMode(); //  we have access to the theme and colorMode now
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          Hello world
+          <main className="content"></main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
