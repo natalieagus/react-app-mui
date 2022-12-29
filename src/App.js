ace5c067459b700a3456d6835ad8feca73e62f47
Natalie Agus <nn88blue@gmail.com>
2022-12-29 20:39:41 +0800

    feat: add Team page to utilise DataGrid from MUI

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import Topbar from "./scenes/global/Topbar";
 import Dashboard from "./scenes/dashboard";
 import Sidebar from "./scenes/global/Sidebar";
+import Team from "./scenes/team";
 
 function App() {
   const [theme, colorMode] = useMode(); //  we have access to the theme and colorMode now
@@ -18,6 +19,7 @@ function App() {
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
             </Routes>
           </main>
         </div>
