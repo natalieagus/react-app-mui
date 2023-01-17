4cfea2f98cc5a41c35f8cd856bd8a78f21083c05
Natalie Agus <nn88blue@gmail.com>
2023-01-17 13:43:53 +0800

    feat: add github baseurl

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="react-app-mui">
       <App />
     </BrowserRouter>
   </React.StrictMode>
